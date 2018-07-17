00f38baf6ed69cdb1ee55dad44ce4f48bcf2ce17
augusta-Ranjith <>
2018-07-17 11:02:50 +0530

    Added web service class

---
+++ AugustaFramework/AUWebService.swift
@@ -0,0 +1,61 @@
+//
+//  AUWebService.swift
+//  AugustaFramework
+//
+//  Created by augusta on 17/07/18.
+//  Copyright © 2018 augusta. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+public typealias kModelSuccessBlock = (_ success : Bool, _ message : String, _ responseObject:AnyObject) ->()
+public typealias kModelErrorBlock = (_ errorMesssage: String) -> ()
+
+public enum AUWebServiceType {
+    case put
+    case get
+    case delete
+    case post
+}
+
+public class AUWebService{
+    
+    public class func callServiceAndGetData(url: String,
+                               type: AUWebServiceType,
+                               userData: [String: Any],
+                               successBlock: @escaping kModelSuccessBlock,
+                               failureBlock : @escaping kModelErrorBlock){
+        
+        var httpMethod: HTTPMethod?
+        
+        // type
+        switch type {
+        case .put:
+            httpMethod = .put
+        case .get:
+            httpMethod = .get
+        case .delete:
+            httpMethod = .delete
+        case .post:
+            httpMethod = .post
+        }
+        
+        Alamofire.request(url, method: httpMethod!, parameters: userData, encoding: JSONEncoding.default, headers: nil).responseJSON { (response:DataResponse<Any>) in
+            
+            switch(response.result) {
+            case .success(_):
+                if response.result.value != nil{
+                    successBlock(true, "success", response.result.value as AnyObject)
+                }
+                break
+                
+            case .failure(let error):
+                failureBlock(error.localizedDescription as String)
+                break
+            }
+        }
+    }
+    
+}
+
---
+++ Pods/Target Support Files/Pods-AugustaFramework/Pods-AugustaFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AugustaFramework : NSObject
+@end
+@implementation PodsDummy_Pods_AugustaFramework
+@end
